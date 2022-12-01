d46e8c11012d97b87935575e40c3bcc14841ab19
Yun Jay Kim <yunjaysus@gmail.com>
2022-12-01 16:34:16 +0900

    Update bayer16.fs

--- shaders/bayer16.fs
+++ shaders/bayer16.fs
@@ -14,7 +14,7 @@ uniform vec2 resolution;
 
 uniform float diffuseScale;
 //threshold map
-const int bayer16[16] = int[](0,  8,  2,  10,
+const int bayer16[16] = int[16](0,  8,  2,  10,
                             12, 4,  14, 6,
                             3,  11, 1,  9,
                             15, 7,  13, 5);
